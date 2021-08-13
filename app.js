723df0ed32ade2d6b6b1b799c664a7431c370866
CyrilBaah <cyril.baah@amalitech.org>
2021-08-13 11:50:12 +0000

    Template Engine ejs

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 require('dotenv').config();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 
-app.get('/', (req, res) => {
-    res.send('works')
+app.get('/', async (req, res) => {
+    res.render('index');
 })
 
 
