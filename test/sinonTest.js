03228d11a4628ce43a35e6e32eb2de654268ff7e
CyrilBaah <cyril.baah@amalitech.org>
2021-08-30 15:42:46 +0000

    Sinon Test

---
+++ test/sinonTest.js
@@ -0,0 +1,27 @@
+const sinon = require("sinon");
+const sinonTest = require("sinon-test");
+const test = sinonTest(sinon);
+ assert = require("assert");
+
+ const Calculator = require('../controller/calculator')
+
+ describe('Calculator', ()=> {
+    let calculator, callbackSpy, clock;
+
+    before(function () {
+        calculator = new Calculator(4, 5);
+        clock = sinon.useFakeTimers();
+        callbackSpy = sinon.spy();
+      });    
+
+      it("should emit the event before the callback", function (done) {
+        calculator.product(callbackSpy);
+        clock.tick(100);
+        assert.equal(callbackSpy.called, true);
+        done();
+      });
+    
+      after(function () {
+        clock.restore();
+      });
+})
